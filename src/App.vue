ad867f635a454b9b7e50b6f4d6c4530056198a78
achiengcatherine <achiengcatherine431@gmail.com>
2023-06-30 14:54:25 +0300

    uploading file

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div class="content">
+   <TodoList/>
+  </div>
+ 
+</template>
+
+
+<script setup lang="ts">
+import TodoList from './components/TodoList.vue'
+import './TodoList.css'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+<div class="todo">
+  <h2>TODO-LIST</h2>
+  <div class="task">
+  <input type="text" placeholder="Enter New Task"/>
+  <button class="btn">ADD</button>
+  </div>
+  <hr/>
+</div>
+
+</template>
+
+<style scoped></style>
